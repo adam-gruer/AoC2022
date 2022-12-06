ed2b703f1ca0b5ce455fa700728b737f31c35530
Adam Gruer <adam.gruer@epa.vic.gov.au>
2022-12-06 19:16:51 +1100

    update 06 recursive function

--- 06.R
+++ 06.R
@@ -8,7 +8,7 @@ input <- strsplit(input, "")[[1]]
 # Helper function(s) --------------------------------------------------------
 
 
-detect_packet <- function(input, packet = character(0), characters_processed = 0) {
+detect_packet <- function(input, packet = character(0), characters_processed = 0, n_char = 4) {
   # end of input reached. exit recursion
   if (length(input) == 0) {
     return(list(char_count = NA_integer_, char = "no start of packet marker received"))
@@ -17,7 +17,7 @@ detect_packet <- function(input, packet = character(0), characters_processed = 0
   # check if packet is free of duplicates
 
   # if so stop recurring & return result
-  if (length(packet) == 4 & anyDuplicated(packet) == 0) {
+  if (length(packet) == n_char & anyDuplicated(packet) == 0) {
     return(list(char_count = characters_processed, char = tail(packet,1)))
 
   }
@@ -30,19 +30,20 @@ detect_packet <- function(input, packet = character(0), characters_processed = 0
   # remove first letter from input
   input <- input[-1]
 
-   # increment characters_rpocessed
+  # increment characters_rpocessed
   characters_processed <- characters_processed + 1
 
   #update packet
-  if (length(packet) < 4){
+  if (length(packet) < n_char){
     new_packet <- c(packet, new_char)} else {
-  new_packet <- c(packet[-1], new_char)
+      new_packet <- c(packet[-1], new_char)
     }
 
   #recursion
- detect_packet(input, new_packet, characters_processed)
+  detect_packet(input, new_packet, characters_processed, n_char)
 
 }
+
 
 detect_packet2 <- function(input, n_char = 4){
 
@@ -69,7 +70,7 @@ detect_packet2(input)
 
 # Part 2 ------------------------------------------------------------------
 
-#detect_packet(input)
+#detect_packet(input, n_char = 14)
 #Error: C stack usage  15924544 is too close to the limit
 
-detect_packet2(input, 14)
+detect_packet2(input, n_char = 14)
